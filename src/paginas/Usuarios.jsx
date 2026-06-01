e15c661c7fd4622a2c1392dbcba81fdaaf4d17e6
filipiquadros <filipi_quadros@estudante.sesisenai.org.br>
2026-06-01 11:51:20 -0300

    Adicionamos a Pocibilidade de editar o Usuario existente porem não finalizamos ela

--- src/paginas/Usuarios.jsx
+++ src/paginas/Usuarios.jsx
@@ -2,7 +2,8 @@ import { useEffect,useState } from "react"
 
 export default function Usuarios() {
     const [cont, setCont] = useState(0)
-   // const [usuarios, setUsuarios] = useStates([])
+    const [usuarios, setUsuarios] = useState([])
+    const [modal, setModal] = useState(false)
 
     const aumentar = () => {
         setCont(cont + 1)
@@ -22,22 +23,50 @@ export default function Usuarios() {
         }}, [cont])
         
         useEffect(() => {
-
+            document.title = `count: ${cont}`
         const buscarUsuario = async () => {
             const resposta = await fetch('http://localhost:3000/usuarios')
-            const usuarios = await resposta.json()
-            console.log(usuarios);
+            const data = await resposta.json()
+            console.log(data);
+            setUsuarios(data)
 
         }
         buscarUsuario()
     }, [cont])
 
+    const editar = (usuario) => {
+        console.log('editando', usuario);
+        setModal(true)
+    }
+
     return (
         <div>
             <h1>Usuarios</h1>
-            {cont}<br />
+            {cont}<br/>
             <button onClick={() => aumentar()}>Aumentar</button>
             <button onClick={() => diminuir()}>Diminuir</button>
+
+            <br/>
+            <br/>
+            
+            <ul>
+
+                {usuarios.map((usuario) => (
+                    <li key={usuario.id}>
+                        {usuario.nome},
+                        {usuario.email},
+                        {usuario.senha}                 <button onClick={editar}>Editar</button>
+                        </li>
+                ))}
+
+            </ul>
+            {modal && (
+                <div className="fundo-modal" onClick={() => setModal(false)}>
+                    <div className="modal-content">
+                    <h1>Editar</h1>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
